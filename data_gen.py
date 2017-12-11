15d9d7a35ccf4140ac0cfca5bbd1a337877799b6
nuric <nuric@users.noreply.github.com>
2017-12-11 15:03:49 +0000

    Working on basic variable case, refactored printing.

--- data_gen.py
+++ data_gen.py
@@ -6,7 +6,7 @@ import random
 parser = argparse.ArgumentParser(description="Generate logic program data.")
 parser.add_argument("task", help="The task to generate.")
 parser.add_argument("size", type=int, help="Number of programs to generate.")
-parser.add_argument("-cs", "--context_size", default=4, type=int, help="Size of program context.")
+parser.add_argument("-cs", "--context_size", default=6, type=int, help="Size of program context.")
 parser.add_argument("-cl", "--constant_length", default=1, type=int, help="Length of constants.")
 parser.add_argument("-vl", "--variable_length", default=1, type=int, help="Length of variables.")
 parser.add_argument("-pl", "--predicate_length", default=1, type=int, help="Length of predicates.")
@@ -32,10 +32,10 @@ def r_string(symbols, length):
 
 def r_symbols(size, symbols, length):
   """Return unique random from given symbols."""
-  r = set()
-  while len(r) < size:
-    r.add(r_string(symbols, length))
-  return list(r)
+  rset = set()
+  while len(rset) < size:
+    rset.add(r_string(symbols, length))
+  return list(rset)
 
 def r_consts(size):
   """Return size many unique constants."""
@@ -49,18 +49,27 @@ def r_preds(size):
   """Return size many unique predicates."""
   return r_symbols(size, PRED_SYMBOLS, ARGS.predicate_length)
 
-def writep(pred):
+def write_p(pred):
   """Format single predicate tuple into string."""
   return PRED_T.format(pred[0], ARG_SEP.join(pred[1]))
 
-def write(preds, isquery=False):
-  """Convert single predicate tuple into string."""
-  head = writep(preds[0])
+def write_r(preds):
+  """Convert rule predicate tuple into string."""
+  head = write_p(preds[0])
   # Is it just a fact
   if len(preds) == 1:
-    return head if isquery else FACT_T.format(head)
+    return FACT_T.format(head)
   # We have a rule
-  return RULE_T.format(head, PRED_SEP.join([writep(p) for p in preds[1:]]))
+  return RULE_T.format(head, PRED_SEP.join([write_p(p) for p in preds[1:]]))
+
+def output(context, targets):
+  """Print the context and given targets."""
+  # context: [[('p', ['a', 'b'])], ...]
+  # targets: [(('p', ['a', 'b']), 1), ...]
+  random.shuffle(context)
+  print('\n'.join([write_r(c) for c in context]))
+  for t, v in targets:
+    print(TARGET_T.format(write_p(t), v))
 
 def gen_task1(context_size):
   """Ground instances only."""
@@ -73,18 +82,40 @@ def gen_task1(context_size):
     ctx.append([(preds[i], args)])
   for i in range(context_size//2, context_size):
     ctx.append([(preds[i], [random.choice(consts[:-1])])])
-  random.shuffle(ctx)
-  print('\n'.join([write(c) for c in ctx]))
   # Successful case when query appears in context
-  target_ts = random.sample(ctx, 2)
-  for t in target_ts:
-    print(TARGET_T.format(write(t, True), 1))
+  targets = list()
+  for t in random.sample(ctx, 2):
+    targets.append((t[0], 1))
   # Out of context constant fails
-  target_f = (random.choice(preds[context_size//2:-1]), [consts[-1]])
-  print(TARGET_T.format(writep(target_f), 0))
+  targets.append(((random.choice(preds[:context_size//2]), [random.choice(consts), consts[-1]]), 0))
   # Out of context predicate fails
-  target_f = (preds[-1], [random.choice(consts[:-1])])
-  print(TARGET_T.format(writep(target_f), 0))
+  targets.append(((preds[-1], [random.choice(consts[:-1])]), 0))
+  output(ctx, targets)
+
+def gen_task2(context_size):
+  """Variablised facts only, no rules."""
+  preds = r_preds(context_size+1)
+  consts = r_consts(context_size+1)
+  var = r_vars(context_size)
+  ctx = list()
+  # Double variable argument
+  for i in range(context_size//3):
+    args = random.sample(var, 2)
+    ctx.append([(preds[i], args)])
+  # Single variable argument
+  for i in range(context_size//3, context_size//3*2):
+    ctx.append([(preds[i], [random.choice(var)])])
+  # Some ground instances
+  for i in range(context_size//3*2, context_size):
+    ctx.append([(preds[i], [random.choice(consts)])])
+  # Ground case
+  targets = [(ctx[-1][0], 1)]
+  # Successful variable grounding
+  p = ctx[0][0][0]
+  targets.append(((p, [random.choice(consts), random.choice(consts)]), 1))
+  # Out of context predicate fails
+  targets.append(((preds[-1], [random.choice(consts[:-1])]), 0))
+  output(ctx, targets)
 
 if __name__ == '__main__':
   task = "gen_task" + ARGS.task
