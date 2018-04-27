52ecda3ac7e40b680413930074ea4eca56f52a2c
nuric <nuric@users.noreply.github.com>
2018-04-27 21:42:30 +0100

    Moved IMA model to sigmoid activation, added dim as parameter.

--- models/ima.py
+++ models/ima.py
@@ -4,9 +4,6 @@ import keras.backend as K
 import keras.layers as L
 from keras.models import Model
 
-PRED_DIM = 64
-STATE_DIM = PRED_DIM
-
 # pylint: disable=line-too-long
 
 class ZeroGRUCell(L.GRUCell):
@@ -86,7 +83,7 @@ class NestedTimeDist(L.TimeDistributed):
                           unroll=False)
     return outputs
 
-def build_model(char_size=27, iterations=4, training=True, ilp=False, pca=False):
+def build_model(char_size=27, dim=64, iterations=4, training=True, ilp=False, pca=False):
   """Build the model."""
   # Inputs
   # Context: (rules, preds, chars,)
@@ -111,25 +108,26 @@ def build_model(char_size=27, iterations=4, training=True, ilp=False, pca=False)
     embedded_ctx = L.Lambda(lambda xs: K.concatenate(xs, axis=1), name='template_concat')([templates, embedded_ctx])
     # embedded_ctx = L.concatenate([templates, embedded_ctx], axis=1)
 
-  embed_pred = ZeroGRU(PRED_DIM, go_backwards=True, name='embed_pred')
-  embedded_predq = embed_pred(embedded_q) # (?, PRED_DIM)
+  embed_pred = ZeroGRU(dim, go_backwards=True, name='embed_pred')
+  embedded_predq = embed_pred(embedded_q) # (?, dim)
   # For every rule, for every predicate, embed the predicate
   embedded_ctx_preds = NestedTimeDist(NestedTimeDist(embed_pred, name='nest1'), name='nest2')(embedded_ctx)
-  # (?, rules, preds, PRED_DIM)
+  # (?, rules, preds, dim)
 
-  # embed_rule = ZeroGRU(PRED_DIM*2, go_backwards=True, name='embed_rule')
+  # embed_rule = ZeroGRU(dim*2, go_backwards=True, name='embed_rule')
   # embedded_rules = NestedTimeDist(embed_rule, name='d_embed_rule')(embedded_ctx_preds)
   get_heads = L.Lambda(lambda x: x[:, :, 0, :], name='rule_heads')
   embedded_rules = get_heads(embedded_ctx_preds)
-  # (?, rules, PRED_DIM)
+  # (?, rules, dim)
 
   # Reused layers over iterations
   repeat_toctx = L.RepeatVector(K.shape(embedded_ctx)[1], name='repeat_to_ctx')
-  diff_sq = L.Lambda(lambda xy: K.square(xy[0]-xy[1]), output_shape=(None, STATE_DIM), name='diff_sq')
-  att_dense = L.TimeDistributed(L.Dense(1, name='att_dense'), name='d_att_dense')
+  diff_sq = L.Lambda(lambda xy: K.square(xy[0]-xy[1]), output_shape=(None, dim), name='diff_sq')
+  att_dense = L.TimeDistributed(L.Dense(1, activation='sigmoid', name='att_dense'), name='d_att_dense')
   squeeze2 = L.Lambda(lambda x: K.squeeze(x, 2), name='sequeeze2')
+  rule_mask = L.Lambda(lambda x: K.cast(K.any(K.not_equal(x, 0), axis=-1, keepdims=True), 'float32'), name='rule_mask')(embedded_rules)
 
-  unifier = NestedTimeDist(ZeroGRU(STATE_DIM, name='unifier'), name='dist_unifier')
+  unifier = NestedTimeDist(ZeroGRU(dim, name='unifier'), name='dist_unifier')
   # gating = L.Dense(1, activation='sigmoid', name='gating')
   # gate2 = L.Lambda(lambda xyg: xyg[2]*xyg[0] + (1-xyg[2])*xyg[1], name='gate')
 
@@ -138,18 +136,19 @@ def build_model(char_size=27, iterations=4, training=True, ilp=False, pca=False)
   outs = list()
   for _ in range(iterations):
     # Compute attention between rule and query state
-    ctx_state = repeat_toctx(state) # (?, rules, STATE_DIM)
+    ctx_state = repeat_toctx(state) # (?, rules, dim)
     s_s_c = diff_sq([ctx_state, embedded_rules])
-    s_m_c = L.multiply([embedded_rules, state]) # (?, rules, PRED_DIM)
+    s_m_c = L.multiply([embedded_rules, state]) # (?, rules, dim)
     sim_vec = L.concatenate([s_s_c, s_m_c])
     sim_vec = att_dense(sim_vec) # (?, rules, 1)
+    sim_vec = L.multiply([sim_vec, rule_mask])
     sim_vec = squeeze2(sim_vec) # (?, rules)
-    sim_vec = L.Softmax(axis=1)(sim_vec)
+    # sim_vec = L.Softmax(axis=1)(sim_vec)
     outs.append(sim_vec)
 
     # Unify every rule and weighted sum based on attention
     new_states = unifier(embedded_ctx_preds, initial_state=[state])
-    # (?, rules, STATE_DIM)
+    # (?, rules, dim)
     state = L.dot([sim_vec, new_states], (1, 1))
 
     # Apply gating
