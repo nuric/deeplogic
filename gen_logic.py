1a6a0bb2ac76d8705431223cb7917eade14cbbd6
nuric <nuric@users.noreply.github.com>
2018-12-12 19:56:29 +0000

    Added OR branching to the new dataset generation, requires verification.

--- gen_logic.py
+++ gen_logic.py
@@ -76,88 +76,108 @@ def output(context, targets):
   for t, v in targets:
     print(TARGET_T.format(write_r([t]), v))
 
-def generate(depth=0, branch=0, context=None, target=None,
-             success=None, upreds=None, uconsts=None):
+def generate(depth=0, context=None, target=None, success=None,
+             upreds=None, uconsts=None, stats=None):
   """Generate tree based logic program."""
   ctx = context or list()
   t = target or [r_preds(1)[0]] + r_consts(R.randint(1, ARGS.arity))
   succ = success if success is not None else R.choice((True, False))
   upreds = upreds or set([t[0]])
   uconsts = uconsts or set(t[1:])
-  # Base case
-  if depth == 0:
-    if R.random() < 0.5:
-      # The constant doesn't match
-      for _ in range(R.randint(1, ARGS.max_or_branch)):
+  stats = stats or dict()
+
+  # Create rule OR branching
+  num_rules = R.randint(1, ARGS.max_or_branch)
+  stats.setdefault('or_num', list()).append(num_rules)
+  # If the rule succeeds than at least one branch must succeed
+  succs = [R.choice((True, False)) for _ in range(num_rules)] \
+          if succ else [False]*num_rules # otherwise all branches must fail
+  if succ and not any(succs):
+    # Ensure at least one OR branch succeeds
+    succs[R.randrange(len(succs))] = True
+  depths = [R.randint(0, depth) if depth > 0 else 0 for _ in range(num_rules)]
+  if max(depths) != depth:
+    depths[R.randrange(num_rules)] = depth
+  print("HERE:", num_rules, succs, depths, t)
+
+  # Generate OR branches
+  is_tadded = False
+  for child_depth, child_succ in zip(depths, succs):
+    # Base case
+    if child_depth == 0:
+      if R.random() < 0.5:
+        # The constant doesn't match
         args = t[1:]
         args[R.randrange(len(args))] = r_consts(1, uconsts)[0]
         uconsts.update(args)
         ctx.append([[t[0]] + args])
-    if R.random() < 0.5:
-      # The predicate doesn't match
-      p = r_preds(1, upreds)[0]
-      upreds.add(p)
-      ctx.append([[p,] + t[1:]])
-    # The predicate doesn't appear at all
-    if succ:
       if R.random() < 0.5:
-        # p(X). case
-        ctx.append([[t[0]] + r_vars(len(t[1:]))])
-      else:
-        # ground case
-        ctx.append([t])
-    return ctx, [(t, int(succ))]
-  # Recursive case
-  num_body = R.randint(1, ARGS.max_and_branch)
-  negation = [R.choice((True, False)) for _ in range(num_body)] \
-             if ARGS.negation else [False]*num_body
-  # Create rule
-  body_preds = r_preds(num_body, upreds)
-  upreds.update(body_preds)
-  arity = len(t[1:])
-  lit_vars = r_vars(arity)
-  lit_vars.extend([r_vars(1)[0] for _ in range(ARGS.unbound_vars)])
-  rule = [[t[0]]+lit_vars[:arity]]
-  vcmap = {lit_vars[i]:t[i+1] for i in range(arity)}
-  targets = list()
-  for i in range(num_body):
-    R.shuffle(lit_vars)
-    pred = [body_preds[i]] + lit_vars[:arity]
-    rule.append([(NEG_PREFIX if negation[i] else "") + pred[0]] + pred[1:])
-    vs = [vcmap.get(v, r_consts(1, uconsts)[0]) for v in lit_vars[:arity]]
-    targets.append([pred[0]]+vs)
-  ctx.append(rule)
-  # Compute recursive targets
-  succ_targets = [R.choice((True, False)) for _ in range(num_body)] \
-                 if not succ else [not n for n in negation]
-  if not succ:
-    # Ensure a failed target
-    ri = R.randrange(len(succ_targets))
-    # succeeding negation fails this, vice versa
-    succ_targets[ri] = negation[ri]
-  # Recurse
-  for target, s in zip(targets, succ_targets):
-    generate(depth-1, branch, ctx, target, s, upreds, uconsts)
-  return ctx, [(t, int(succ))]
+        # The predicate doesn't match
+        p = r_preds(1, upreds)[0]
+        upreds.add(p)
+        ctx.append([[p,] + t[1:]])
+      # The predicate doesn't appear at all
+      if child_succ:
+        if R.random() < 0.5:
+          # p(X). case
+          ctx.append([[t[0]] + r_vars(len(t[1:]))])
+        elif not is_tadded:
+          # ground case
+          ctx.append([t])
+          is_tadded = True
+      continue
+    # Recursive case
+    num_body = R.randint(1, ARGS.max_and_branch)
+    stats.setdefault('body_num', list()).append(num_body)
+    negation = [R.choice((True, False)) for _ in range(num_body)] \
+               if ARGS.negation else [False]*num_body
+    # Create rule
+    body_preds = r_preds(num_body, upreds)
+    upreds.update(body_preds)
+    arity = len(t[1:])
+    lit_vars = r_vars(arity)
+    lit_vars.extend([r_vars(1)[0] for _ in range(ARGS.unbound_vars)])
+    rule = [[t[0]]+lit_vars[:arity]]
+    vcmap = {lit_vars[i]:t[i+1] for i in range(arity)}
+    targets = list()
+    for i in range(num_body):
+      R.shuffle(lit_vars)
+      pred = [body_preds[i]] + lit_vars[:arity]
+      rule.append([(NEG_PREFIX if negation[i] else "") + pred[0]] + pred[1:])
+      vs = [vcmap.get(v, r_consts(1, uconsts)[0]) for v in lit_vars[:arity]]
+      targets.append([pred[0]]+vs)
+    ctx.append(rule)
+    # Compute recursive targets
+    succ_targets = [R.choice((True, False)) for _ in range(num_body)] \
+                   if not child_succ else [not n for n in negation]
+    if not child_succ:
+      # Ensure a failed target
+      ri = R.randrange(len(succ_targets))
+      # succeeding negation fails this, vice versa
+      succ_targets[ri] = negation[ri]
+    # Recurse
+    for target, s in zip(targets, succ_targets):
+      generate(child_depth-1, ctx, target, s, upreds, uconsts, stats)
+  return ctx, [(t, int(succ))], stats
 
 if __name__ == '__main__':
   # Arguments
   parser = argparse.ArgumentParser(description="Generate logic program data.")
   parser.add_argument("-d", "--depth", default=0, type=int, help="The depth of the logic program.")
-  parser.add_argument("-b", "--branch", default=0, type=int, help="Branches in the logic program.")
-  parser.add_argument("-mob", "--max_or_branch", default=2, type=int, help="Upper bound on number of branches.")
-  parser.add_argument("-mab", "--max_and_branch", default=2, type=int, help="Upper bound on number of branches.")
+  parser.add_argument("-mob", "--max_or_branch", default=1, type=int, help="Upper bound on number of branches.")
+  parser.add_argument("-mab", "--max_and_branch", default=1, type=int, help="Upper bound on number of branches.")
   parser.add_argument("-s", "--size", default=1, type=int, help="Number of programs to generate.")
   # Configuration parameters
-  parser.add_argument("-uv", "--unbound_vars", default=2, type=int, help="Number of unbound variables.")
+  parser.add_argument("-uv", "--unbound_vars", default=0, type=int, help="Number of unbound variables.")
   parser.add_argument("-ar", "--arity", default=2, type=int, help="Upper bound on arity of literals.")
   parser.add_argument("-n", "--negation", action="store_true", help="Use negation by failure.")
-  parser.add_argument("-cl", "--constant_length", default=1, type=int, help="Length of constants.")
+  parser.add_argument("-cl", "--constant_length", default=2, type=int, help="Length of constants.")
   parser.add_argument("-vl", "--variable_length", default=1, type=int, help="Length of variables.")
-  parser.add_argument("-pl", "--predicate_length", default=1, type=int, help="Length of predicates.")
+  parser.add_argument("-pl", "--predicate_length", default=2, type=int, help="Length of predicates.")
   parser.add_argument("-sf", "--shuffle_context", action="store_true", help="Shuffle context before output.")
-  parser.add_argument("-ts", "--templates", nargs='*', type=int, help="Templates to use.")
   ARGS = parser.parse_args()
 
   for _ in range(ARGS.size):
-    output(*generate(depth=ARGS.depth, branch=ARGS.branch))
+    context, targets, stats = generate(depth=ARGS.depth)
+    output(context, targets)
+    print(stats)
