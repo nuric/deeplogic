d21d7bd312b7b66593d144e6291b31b7924ed0e5
nuric <nuric@users.noreply.github.com>
2020-07-06 20:08:59 +0100

    Upgrade zerogru implementation to latest keras tensorflow version.

--- models/zerogru.py
+++ models/zerogru.py
@@ -1,4 +1,5 @@
 """ZeroGRU module for nested RNNs."""
+import keras
 import keras.backend as K
 import keras.layers as L
 
@@ -7,39 +8,37 @@ class ZeroGRUCell(L.GRUCell):
   def call(self, inputs, states, training=None):
     """Step function of the cell."""
     h_tm1 = states[0] # previous output
-    cond = K.all(K.equal(inputs, 0), axis=-1)
+    cond = K.all(K.equal(inputs, 0), axis=-1, keepdims=True)
     new_output, new_states = super().call(inputs, states, training=training)
     curr_output = K.switch(cond, h_tm1, new_output)
     curr_states = [K.switch(cond, states[i], new_states[i]) for i in range(len(states))]
     return curr_output, curr_states
 
 
-class ZeroGRU(L.GRU):
-  """Layer wrapper for the ZeroGRUCell."""
-  def __init__(self, units,
-               activation='tanh',
-               recurrent_activation='hard_sigmoid',
-               use_bias=True,
-               kernel_initializer='glorot_uniform',
-               recurrent_initializer='orthogonal',
-               bias_initializer='zeros',
-               kernel_regularizer=None,
-               recurrent_regularizer=None,
-               bias_regularizer=None,
-               activity_regularizer=None,
-               kernel_constraint=None,
-               recurrent_constraint=None,
-               bias_constraint=None,
-               dropout=0.,
-               recurrent_dropout=0.,
-               implementation=1,
-               return_sequences=False,
-               return_state=False,
-               go_backwards=False,
-               stateful=False,
-               unroll=False,
-               reset_after=False,
-               **kwargs):
+def ZeroGRU(units,
+            activation='tanh',
+            recurrent_activation='hard_sigmoid',
+            use_bias=True,
+            kernel_initializer='glorot_uniform',
+            recurrent_initializer='orthogonal',
+            bias_initializer='zeros',
+            kernel_regularizer=None,
+            recurrent_regularizer=None,
+            bias_regularizer=None,
+            kernel_constraint=None,
+            recurrent_constraint=None,
+            bias_constraint=None,
+            dropout=0.0,
+            recurrent_dropout=0.0,
+            implementation=1,
+            return_sequences=False,
+            return_state=False,
+            go_backwards=False,
+            stateful=False,
+            unroll=False,
+            reset_after=False,
+            **kwargs):
+    """Layer wrapper for the ZeroGRUCell."""
     cell = ZeroGRUCell(units,
                        activation=activation,
                        recurrent_activation=recurrent_activation,
@@ -57,27 +56,39 @@ class ZeroGRU(L.GRU):
                        recurrent_dropout=recurrent_dropout,
                        implementation=implementation,
                        reset_after=reset_after)
-    super(L.GRU, self).__init__(cell,
-                                return_sequences=return_sequences,
-                                return_state=return_state,
-                                go_backwards=go_backwards,
-                                stateful=stateful,
-                                unroll=unroll,
-                                **kwargs)
-    self.activity_regularizer = L.regularizers.get(activity_regularizer)
+    return L.RNN(cell, return_sequences=return_sequences,
+                 return_state=return_state,
+                 go_backwards=go_backwards,
+                 stateful=stateful,
+                 unroll=unroll)
 
-class NestedTimeDist(L.TimeDistributed):
+def collapse(inputs):
+  """Collapse higher dimensions."""
+  shape = K.shape(inputs)
+  batch_times_rest = K.prod(shape[:-2], keepdims=True)  # ()
+  new_shape = K.concatenate(
+      [batch_times_rest, shape[-2:]]
+  )  # (B*X, timesteps, features)
+  return K.reshape(inputs, new_shape)
+
+def expand(inputs):
+  """Expand back into higher dimensions."""
+  original_shape = K.shape(inputs[1])  # (B, ..., timesteps, features)
+  output_shape = K.shape(inputs[0])  # (B*X, features)
+  new_shape = K.concatenate([original_shape[:-2], output_shape[-1:]])
+  return K.reshape(inputs[0], new_shape)
+
+collapseL = L.Lambda(collapse, name='collapseL')
+expandL = L.Lambda(expand, name='expandL')
+
+
+def NestedTimeDist(layer, **kwargs):
   """Nested TimeDistributed wrapper for higher rank tensors."""
-  def call(self, inputs, mask=None, training=None, initial_state=None):
-    def step(x, _):
-      output = self.layer.call(x, mask=mask,
-                               training=training,
-                               initial_state=initial_state)
-      return output, []
-    _, outputs, _ = K.rnn(step, inputs,
-                          initial_states=[],
-                          unroll=False)
-    return outputs
+  def call(inputs, mask=None, training=None, initial_state=None):
+    """Wrap reshaping around RNN layer call."""
+    nonlocal layer
+    collapsed = collapseL(inputs)
+    layer_out = layer(collapsed)
+    return expandL([layer_out, inputs])
 
-  def compute_mask(self, inputs, mask=None):
-    return None
+  return call
